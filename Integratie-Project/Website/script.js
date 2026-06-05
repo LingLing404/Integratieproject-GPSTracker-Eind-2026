ee430bc3bc1207d7af69e970ddebe9dd21d8a7f4
upsting <jimxquo@gmail.com>
2026-06-05 11:19:41 +0200

    Remove website files

--- Integratie-Project/Website/script.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector("#login-form");
-const message = document.querySelector("#form-message");
-const params = new URLSearchParams(window.location.search);
-
-if (params.get("error")) {
-    message.textContent = "Login mislukt. Controleer je e-mailadres en wachtwoord.";
-}
